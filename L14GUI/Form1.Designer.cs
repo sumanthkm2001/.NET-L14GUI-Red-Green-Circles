6387c3086c7b0a5603f701351cd91c295328724f
User <User@SUMANTH>
2023-09-29 02:41:53 +0530

    Add project files.

---
+++ L14GUI/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace L14GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdGreen = new System.Windows.Forms.Button();
+            this.cmdRed = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmdGreen
+            // 
+            this.cmdGreen.Location = new System.Drawing.Point(13, 13);
+            this.cmdGreen.Name = "cmdGreen";
+            this.cmdGreen.Size = new System.Drawing.Size(75, 23);
+            this.cmdGreen.TabIndex = 0;
+            this.cmdGreen.Text = "Green";
+            this.cmdGreen.UseVisualStyleBackColor = true;
+            this.cmdGreen.Click += new System.EventHandler(this.cmdGreen_Click);
+            // 
+            // cmdRed
+            // 
+            this.cmdRed.Location = new System.Drawing.Point(13, 53);
+            this.cmdRed.Name = "cmdRed";
+            this.cmdRed.Size = new System.Drawing.Size(75, 23);
+            this.cmdRed.TabIndex = 1;
+            this.cmdRed.Text = "Red";
+            this.cmdRed.UseVisualStyleBackColor = true;
+            this.cmdRed.Click += new System.EventHandler(this.cmdRed_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmdRed);
+            this.Controls.Add(this.cmdGreen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdGreen;
+        private System.Windows.Forms.Button cmdRed;
+    }
+}
+
---
+++ L14GUI/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L14GUI
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        Thread tRed;
+        Thread tGreen;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdGreen_Click(object sender, EventArgs e)
+        {
+            tGreen = new Thread(ThreadGreen);
+            tGreen.Start();
+        }
+        public void ThreadGreen()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                this.CreateGraphics().DrawEllipse(new Pen(Brushes.Green, 5), new Rectangle(rand.Next(0, this.Width), rand.Next(0, this.Height), 100, 100));
+                Thread.Sleep(100);
+            }
+            MessageBox.Show("Completed 100 Green ");
+        }
+
+        private void cmdRed_Click(object sender, EventArgs e)
+        {
+            tRed = new Thread(ThreadRed);
+            tRed.Start();
+        }
+        public void ThreadRed()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                this.CreateGraphics().DrawEllipse(new Pen(Brushes.Red, 5), new Rectangle(rand.Next(0, this.Width), rand.Next(0, this.Height), 100, 100));
+                Thread.Sleep(100);
+            }
+            MessageBox.Show("Completed 100 Green ");
+        }
+    }
+}
